c114dfcc7a5bd43c1a5c59bf242b9b8f66ae5bb8
Andrej Pedin <andrejpedin@gmail.com>
2026-06-29 06:20:35 +0000

    Add release signing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,19 @@ android {
         versionName = "4.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASS") ?: "11q22w33e44r"
+            keyAlias = "TimeCalc"
+            keyPassword = System.getenv("KEYSTORE_PASS") ?: "11q22w33e44r"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
